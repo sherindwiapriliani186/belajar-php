bbd1236977edebde1467cb6128633b1734af335e
Sherin Dwi Apriliani <sherindwi05@webmail.umm.ac.id>
2022-10-26 21:04:46 +0700

    belajar php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+   
+   echo"Hellow World !<br>"; 
+
+    $nama = "Sherin" ;
+    $umur = 21 ;
+   
+    echo"Nama saya $nama, saya berusia $umur <br>";
+
+    $namaadik = "adit";
+    $umuradik = 15;
+
+    echo"Nama adik saya $namaadik, adik saya berusia $umuradik<br>";
+
+    $selisih = $umur-$umuradik;
+    echo "selisih usia saya dan adik saya adalah $selisih tahun<br>";
+?>
